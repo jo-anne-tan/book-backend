39bf522a787e04885e05715911baaf10e9fa9e4b
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-02-18 14:41:23 +0800

    added comments in validate function

--- models/shop_services.py
+++ models/shop_services.py
@@ -3,9 +3,20 @@ from models.shop import Shop
 from models.service import Service
 from models.base_model import BaseModel
 
-class ShopServices(BaseModel):
+class Shop_services(BaseModel):
     shop = pw.ForeignKeyField(Shop, backref="shops")
     service = pw.ForeignKeyField(Service, backref="services")
     price = pw.DecimalField(null=False)
     range_km = pw.IntegerField(default="10") # 10 km radius from shop location
+    door_service = pw.BooleanType(default=False)
+    store_service = pw.BooleanType(default=True)
 
+    def validate(self):
+        pass
+        # add range check 
+            # no negative numbers
+            # no more than 50km
+            # door_service must be True
+        # add price check - no negative numbers
+        # add door_service check
+            # range_km must be Truthy
