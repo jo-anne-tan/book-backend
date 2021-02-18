08ea62a95c649b83d0682ec0e52c67289d6013dd
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-02-18 15:18:23 +0800

    added operating days and time fields

--- models/shop.py
+++ models/shop.py
@@ -13,6 +13,25 @@ class Shop(BaseModel):
     country = pw.CharField(default="Malaysia")
     postcode = pw.CharField(null=False)
 
+    # True for work days, false otherwise
+    # e.g. if shop operates on Monday only, then only Monday is True
+    mon = pw.BooleanField(default=False)
+    tue = pw.BooleanField(default=False)
+    wed = pw.BooleanField(default=False)
+    thu = pw.BooleanField(default=False)
+    fri = pw.BooleanField(default=False)
+    sat = pw.BooleanField(default=False)
+    sun = pw.BooleanField(default=False)
+
+    # Assuming same operating hours on all chosen work days
+    # This is either the full operating hours, or the work session before lunch break
+    start_time_1 = pw.TimeField(null=False)
+    end_time_1 = pw.TimeField(null=False)
+
+    # work session after lunch break. optional.
+    start_time_2 = pw.TimeField()
+    end_time_2 = pw.TimeField()
+
     def validate(self):
         pass
         # add postcode check - 5 digits
