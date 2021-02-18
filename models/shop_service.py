20ecd666e188911fa1a27fd0755fade4055a27d8
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-02-18 15:20:09 +0800

    updated Shop_service model

--- models/shop_service.py
+++ models/shop_service.py
@@ -3,9 +3,9 @@ from models.shop import Shop
 from models.service import Service
 from models.base_model import BaseModel
 
-class Shop_services(BaseModel):
-    shop = pw.ForeignKeyField(Shop, backref="shops")
-    service = pw.ForeignKeyField(Service, backref="services")
+class Shop_service(BaseModel):
+    shop = pw.ForeignKeyField(Shop, backref="shop_services")
+    service = pw.ForeignKeyField(Service, backref="shop_services")
     price = pw.DecimalField(null=False)
     range_km = pw.IntegerField(default="10") # 10 km radius from shop location
     door_service = pw.BooleanType(default=False)
